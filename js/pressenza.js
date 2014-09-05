acc8f19d63e2d4fc69505e2622045fd3c2f80350
Tom Bütikofer <tom.buetikofer@highfive.ch>
2014-09-05 09:08:07 +0200

    solve responisve video embed with jQuery

---
+++ js/pressenza.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($){
+	
+	// Make Youtube and Vimeo responsive
+	// http://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+	var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='http://www.youtube.com']"),
+	$fluidEl = $(".article-view-content");
+	$allVideos.each(function() {
+		$(this).data('aspectRatio', this.height / this.width)
+	    .removeAttr('height')
+	    .removeAttr('width');
+	});
+	$(window).resize(function() {
+	  var newWidth = $fluidEl.width();
+	  $allVideos.each(function() {
+	    var $el = $(this);
+	    $el
+	      .width(newWidth)
+	      .height(newWidth * $el.data('aspectRatio'));
+	  });
+	}).resize();
+	
+});
