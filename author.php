854ff784f99fce084f36310fe98dddbb1c93e60a
Tom Bütikofer <tom.buetikofer@highfive.ch>
2014-11-12 23:52:17 +0100

    add paginator to author page

--- author.php
+++ author.php
@@ -12,13 +12,16 @@ global $wp_query;
 
 $data = Timber::get_context();
 $data['posts'] = Timber::get_posts();
+$data['pagination'] = Timber::get_pagination();
 
+//$curauth = (get_query_var('author_name')) ? get_user_by('slug', get_query_var('author_name')) : get_userdata(get_query_var('author'));
+//print_r($curauth);
+//$user = get_userdata( get_the_author_meta('ID') );
+//print_r($user);
+//exit;
 $author = new TimberUser($wp_query->query_vars['author']);
 $data['author'] = $author;
 $data['pagetitle'] = ' : ' . $author->name();
 $data['title'] = $author->name();
-
-$exp['postedby'] = __('Posted by');
-$data['exp'] = $exp;
 
 Timber::render(array('author.twig', 'archive.twig'), $data);
