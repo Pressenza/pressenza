59297fbad79b615231aa647d8b1987791f9e032c
Tom Bütikofer <tom.buetikofer@highfive.ch>
2015-02-14 21:19:02 +0100

    add twig view for search results

--- search.php
+++ search.php
@@ -13,7 +13,8 @@
 	$templates = array('search.twig', 'archive.twig', 'index.twig');
 	$context = Timber::get_context();
 
-	$context['title'] = 'Search results for '. get_search_query();
+	$context['title'] = get_search_query();
 	$context['posts'] = Timber::get_posts();
+    $context['pagination'] = Timber::get_pagination();
 
 	Timber::render($templates, $context);
